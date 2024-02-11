85183d4f41fa717de9bcffe86cad7043e41341a8
Nikhil Desai <nikhildesai@Nikhils-MBP.bbrouter>
2024-02-11 21:25:07 +0530

    add class method to create Facet filters

--- youtube-media-downloader/facet.py
+++ youtube-media-downloader/facet.py
@@ -1,44 +1,14 @@
 from typing import List, Type, Set, Callable, Any, Collection, Tuple, Optional, Union, Iterable, Mapping, Dict
 from interfaces import FacetFilter, FacetFilterConstraint, Faceted
 
-def __raise(
-        arg: Any,
-        err_cond: Callable[[Any], bool],
-        err_type: Type[BaseException],
-        err_msg: str,
-        ) -> None:
-    if err_cond(arg):
-        raise err_type(err_msg)
-    else:
+class DerivedFacetFilter(FacetFilter):
+
+    @classmethod
+    def from_view_name_and_options(cls, name: str, options: List[Optional[Union[str, int]]]):
         pass
+
+class DerivedFacetFilterConstraint(FacetFilterConstraint):
     
-def __raise_type_error_if_arg_not_string(arg: Any, err_msg: str) -> None:
-    __raise(
-        arg,
-        lambda a: not isinstance(a, str),
-        TypeError,
-        err_msg
-        )
-    
-def __raise_value_error_if_arg_is_empty_string(arg: Any, err_msg: str) -> None:
-    __raise(
-        arg,
-        lambda a: a == "",
-        ValueError,
-        err_msg
-    )
-
-class FacetFilterDerivedFromDataset(FacetFilter):
-    # Since, we are dealing with multiple sub-types
-    # of this and also initializing one of them
-    # this Protocol must specify that it's __init__ can take in a 'str' key
-    def __init__(self, key: str):
-        pass
-
-    def add_option(self, option: Optional[Union[str, int]]) -> None:
-        pass
-
-class FacetFilterConstraintCreatedFromConstraint(FacetFilterConstraint):
     @classmethod
     def from_constraint(cls, constraint: Tuple[str, Any]):
         pass
@@ -46,14 +16,10 @@ class FacetFilterConstraintCreatedFromConstraint(FacetFilterConstraint):
 class FacetFilterWithStrOptions:
 
     def __init__(self, key: str) -> None:
-        if not isinstance(key, str):
-            raise TypeError("Facet key must be of type 'str'")
-        if not key:
-            raise ValueError("Facet key cannot be an empty string")
         self.__key = key
         self.__options: Set[str] = set()
 
-    def add_option(self, option: Optional[Union[str, int]]) -> None:
+    def __add_option(self, option: Optional[Union[str, int]]) -> None:
         if not isinstance(option, str):
             raise TypeError("option must be of type 'str'")
         if not option:
@@ -66,26 +32,23 @@ class FacetFilterWithStrOptions:
     def options(self) -> Collection[str]:
         return list(self.__options)
     
-class SingleSelectionFacetFilterConstraint:
+    @classmethod
+    def from_view_name_and_options(cls, name: str, options: List[Optional[Union[str, int]]]):
+        facet_filter_obj = cls(name)
+        for option in options:
+            facet_filter_obj.__add_option(option)
+        return facet_filter_obj
+    
+class FacetFilterConstraintWithSingleStrSelection:
 
-    def __init__(self, key: str, selected_option: str) -> None:
-        __raise_type_error_if_arg_not_string(
-            key,
-            "Facet constraint key must be of type 'str'"
-            )
-        __raise_type_error_if_arg_not_string(
-            selected_option,
-            "Facet constraint selected option must be of type 'str'"
-        )
-        __raise_value_error_if_arg_is_empty_string(
-            key,
-            "Facet constraint key cannot be an empty string"
-        )
-        __raise_value_error_if_arg_is_empty_string(
-            selected_option,
-            "Facet constraint selected option cannot be an empty string"
-        )
+    def __init__(self, key: str) -> None:
         self.__key = key
+
+    def __set_selected_option(self, selected_option: Any):
+        if not isinstance(selected_option, str):
+            raise TypeError("Facet constraint selected option must be of type 'str'")
+        if not selected_option:
+            raise ValueError("Facet constraint selected option cannot be an empty string")
         self.__selected = selected_option
 
     def key(self) -> str:
@@ -96,57 +59,21 @@ class SingleSelectionFacetFilterConstraint:
     
     @classmethod
     def from_constraint(cls, constraint: Tuple[str, Any]):
-        def _two_tuple_of_strings(c: Any) -> bool:
-            if not isinstance(c, tuple):
-                return True
-            if not len(c) == 2:
-                return True
-            if not all([isinstance(el, str) for el in c]):
-                return True
-            return False
-        __raise(
-            constraint,
-            _two_tuple_of_strings,
-            TypeError,
-            "constraint must be a two-tuple of 'str' type"
-        )
         key, selected_option = constraint
-        return cls(key, selected_option)
+        facet_filter_constraint_obj = cls(key)
+        facet_filter_constraint_obj.__set_selected_option(selected_option)
+        return facet_filter_constraint_obj
 
 class FacetFilterComponentImpl:
 
     def __self__(
             self,
-            facet_filter_types: List[Type[FacetFilterDerivedFromDataset]],
-            facet_filter_constraint_types: List[Type[FacetFilterConstraintCreatedFromConstraint]]
+            facet_filter_types: List[Type[DerivedFacetFilter]],
+            facet_filter_constraint_types: List[Type[DerivedFacetFilterConstraint]]
             ) -> None:
         self.__facet_filter_types = facet_filter_types
         self.__facet_filter_constraint_types = facet_filter_constraint_types
-
-    def __derive_facet_filter_from_key_and_option(self, key: str, option: Optional[Union[str, Collection[str], int]]) -> FacetFilterDerivedFromDataset:
-        for facet_filter_type in self.__facet_filter_types:
-            try:
-                possible_facet_filter_obj = facet_filter_type(key)
-                possible_facet_filter_obj.add_option(option)
-                return possible_facet_filter_obj
-            except:
-                continue
-        raise ValueError()
     
-    def __derive(self, dataset: Iterable[Faceted]) -> Collection[FacetFilter]:
-        facet_filter_for_view: Dict[str, FacetFilterDerivedFromDataset] = dict()
-        for datum in dataset:
-            for facet_view in datum.facet_views():
-                facet_view_value = datum.facet_view_value(facet_view)
-                if facet_view_value is None:
-                    continue
-                if facet_view in facet_filter_for_view.keys():
-                    facet_filter_for_view[facet_view].add_option(facet_view_value)
-                else:
-                    facet_filter_for_view[facet_view] = self.__derive_facet_filter_from_key_and_option(facet_view, facet_view_value)
-        return facet_filter_for_view.values()
-
-
     def derive_facet_filters(self, dataset: Iterable[Faceted]) -> Collection[FacetFilter]:
         facet_filters = self.__derive(dataset)
         return [facet_filter for facet_filter in facet_filters if len(facet_filter.options()) > 1]
